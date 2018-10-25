d80250fadf8af4feb6cab3cac0b21d1d8a386deb
Nick Nissen <nsn@westsoftdevelopment.dk>
2018-10-25 20:50:00 +0200

    Add examples

---
+++ src/examples/DataTableInitialData.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="container mx-auto mt-12 font-sans">
+  <data-table
+    :fetcher="getData"
+    :initial-data="initialData"
+    :use-query-string="false"
+    table-styles="w-full"
+    >
+      <template slot="filters" slot-scope="{ refreshData }">
+        <div class="w-full flex justify-end">
+          <button class="bg-teal-dark text-white py-4 px-2 mb-2" @click="refreshData">
+            Refresh data
+          </button>
+        </div>
+      </template>
+      <template slot="columns" slot-scope="{isSorted, isSortedDesc, isSortedAsc, toggleSort}">
+        <th class="font-thin p-4 border text-left">Navn</th>
+        <th class="font-thin p-4 text-left border border-l-0">Email</th>
+      </template>
+      <template slot-scope="{row}">
+        <td class="font-thin p-4 text-left border">{{ row.name }}</td>
+        <td class="font-thin p-4 text-left border border-l-0">{{ row.email }}</td>
+      </template>
+    </data-table>
+    </div>
+</template>
+<script>
+import DataTable from '../components/DataTable.vue';
+
+export default {
+  components: {
+    DataTable,
+  },
+  data: () => ({
+    initialData: {
+      data: [
+        { email: 'o0sandar0o@mail.dk', name: 'Sandra' },
+        { email: 'aksel@mail.dk', name: 'Aksel' },
+        { email: 'd_bjørn@mail.dk', name: 'Bjørn' },
+        { email: 'bernhard@mail.dk', name: 'Bernhard' },
+      ],
+      totalCount: 4,
+    },
+  }),
+  methods: {
+    getData() {
+      const response = this.initialData;
+      response.data.sort(() => Math.random() - 0.5);
+      return Promise.resolve(response);
+    },
+  },
+};
+</script>
--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -1,26 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import MyButton from '../components/MyButton.vue';
+import SortExample from '../examples/DataTableSort.vue';
+import InitialDataExample from '../examples/DataTableInitialData.vue';
 import '../assets/main.pcss';
 
-storiesOf('Button', module)
-  .add('with text', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action('clicked') },
-  }))
-  .add('with JSX', () => ({
-    components: { MyButton },
-    render() {
-      return <my-button onClick={this.action}>With JSX</my-button>;
-    },
-    methods: { action: linkTo('Button', 'with some emoji') },
-  }))
-  .add('with some emoji', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action('clicked') },
-  }));
+
+storiesOf('DataTable', module)
+  .add(
+    'sorting',
+    () => ({
+      components: { SortExample },
+      template: '<sort-example />',
+    }),
+    { info: '☹️ no emojis' }, // Add additional info text directly
+  )
+  .add(
+    'preloaded with initial data',
+    () => ({
+      components: { InitialDataExample },
+      template: '<initial-data-example />',
+    }),
+  );
