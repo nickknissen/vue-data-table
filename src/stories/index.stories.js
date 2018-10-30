1c0f2837190d7c072e2da96b7664ae78e88ca21e
Nick Nissen <nsn@westsoftdevelopment.dk>
2018-10-30 19:55:02 +0100

    Add more examples

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,12 +3,15 @@ import { storiesOf } from '@storybook/vue';
 
 import SortExample from '../examples/DataTableSort.vue';
 import InitialDataExample from '../examples/DataTableInitialData.vue';
+import FetchDataFromApi from '../examples/FetchDataFromAPI.vue';
+import FilterDataFromApi from '../examples/FilterDataFromAPI.vue';
+
 import '../assets/main.pcss';
 
 
 storiesOf('DataTable', module)
   .add(
-    'sorting',
+    'Sorting',
     () => ({
       components: { SortExample },
       template: '<sort-example />',
@@ -16,9 +19,23 @@ storiesOf('DataTable', module)
     { info: '☹️ no emojis' }, // Add additional info text directly
   )
   .add(
-    'preloaded with initial data',
+    'Preloaded with initial data',
     () => ({
       components: { InitialDataExample },
       template: '<initial-data-example />',
+    }),
+  )
+  .add(
+    'Fetch data from api',
+    () => ({
+      components: { FetchDataFromApi },
+      template: '<fetch-data-from-api />',
+    }),
+  )
+  .add(
+    'Filter data from api',
+    () => ({
+      components: { FilterDataFromApi },
+      template: '<filter-data-from-api />',
     }),
   );
